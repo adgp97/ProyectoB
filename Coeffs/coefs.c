1c7facd06822239d21cc08083f4d86e1fa5ba671
Adrian <adgp1997@hotmail.com>
2019-05-20 14:50:31 -0400

    //testing the way coefficients are taken from Matlab generated header

---
+++ Coeffs/coefs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "fdacoeffs.h"
+
+//testing the way coefficients are capture from Matlab generated header
+//it will print the coefficients to a .txt file
+int main(){
+
+	FILE *fptr;
+
+	fptr = fopen("coeffs.txt","a");
+
+	for (int i = 0; i < BL; i++)
+	{
+		fprintf(fptr, "%f\n",B[i]);
+	}
+
+	fclose(fptr);
+
+	getchar();
+	return 0;
+}
